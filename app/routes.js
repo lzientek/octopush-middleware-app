88d30d245449dc7a3813e5328d21a929957fdddf
lucas zientek <152565@supinfo.com>
2018-01-19 23:26:58 +0100

    fix eslint

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,18 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
-import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import { Route, Switch } from 'react-router';
 
-export default () => (
-  <App>
-    <Switch>
-      <Route path="/counter" component={CounterPage} />
-      <Route path="/" component={HomePage} />
-    </Switch>
-  </App>
+import App from './containers/App';
+import CounterPage from './containers/CounterPage';
+import HomePage from './containers/HomePage';
+
+const Application = () => (
+    <App>
+        <Switch>
+            <Route path="/counter" component={CounterPage} />
+            <Route path="/" component={HomePage} />
+        </Switch>
+    </App>
 );
+
+export default Application;
